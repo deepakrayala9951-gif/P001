704c57fa6158ae79541fbddf88ffe78bc488cc58
deepakrayala9951 <deepakrayala9951@gmail.com>
2026-01-02 10:17:31 +0530

    Initial commit

---
+++ feds2/src/App.jsx
@@ -0,0 +1,11 @@
+import'./App.css';
+function App(){
+  return(
+    <div className='holy'>
+      <h1>Name of series :Stranger things!!</h1><h1>Season: 5</h1><h1>Rating : 9.5/10 (IMDB)</h1>
+
+    </div>
+  );
+}
+
+export default App;
